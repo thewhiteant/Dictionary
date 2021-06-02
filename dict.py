d9b594b93bd78b1f0d3bc420a95249b582f33fb3
White Ant <trafi227@gmail.com>
2021-06-02 19:48:30 +0600

    Update dict.py

--- dict.py
+++ dict.py
@@ -1,15 +1,26 @@
 import json
+from difflib import get_close_matches
 
 
 file  =  open('dictionary.json')
 data = json.load(file)
 
 def retrans(word):
-    return data[word]
-    
+    fword = word.lower()
+    if fword in data:
+        return data[fword]
+    elif len(get_close_matches(fword,data.keys())) > 0 :
+        yn = input(f"Is This Closer To {get_close_matches(fword,data.keys())[0]} ? (Y or N ) :  ")
+        if yn.lower() == "y":
+            return data[get_close_matches(fword, data.keys())[0]]
+        else :
+            return "Sorry Not Found"
+    else:
+        return "Word Not Found"
 
 
 print("<============Whiteant Ting ting Dictionary===========>")
 matchword = input("Word: ")
 result = retrans(matchword)
 print(result)
+
